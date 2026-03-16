d87bab289a16a97f1fce67d63f5476b7b56d67ea
[Rapeepat Boolsuk] <156444362+thaoza12345@users.noreply.github.com>
2026-03-16 15:41:13 +0700

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ res.end('Hello World from DevOps Lab!\n');
 }; 
 const app = http.createServer(requestListener); 
 // We only listen if this file is run directly, not when imported by 
-tests 
+//tests 
 if (require.main === module) { 
 const port = process.env.PORT || 3000; 
 app.listen(port, () => { 
